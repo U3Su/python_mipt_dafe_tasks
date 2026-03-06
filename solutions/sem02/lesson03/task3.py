400a5242a78f07b49d9fafcdac6d0694ad35d55b
U3Su <u3.su@yandex.ru>
2026-03-06 21:34:44 +0300

    task3

--- solutions/sem02/lesson03/task3.py
+++ solutions/sem02/lesson03/task3.py
@@ -3,4 +3,20 @@ import numpy as np
 
 def get_extremum_indices(
     ordinates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray]: ...
+) -> tuple[np.ndarray, np.ndarray]:
+    if len(ordinates) < 3:
+        raise ValueError
+
+    prev = ordinates[:-2]
+    curr = ordinates[1:-1]
+    next = ordinates[2:]
+
+    min_mask = (curr < prev) & (curr < next)
+    max_mask = (curr > prev) & (curr > next)
+
+    indices = np.arange(1, len(ordinates) - 1)
+
+    min_index = indices[min_mask]
+    max_index = indices[max_mask]
+
+    return (min_index, max_index)
